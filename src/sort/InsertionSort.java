4362d8a4cce72ababce0dd4401a70343a309ef5f
charanvm8 <charanvm8@gmail.com>
2019-09-05 18:27:42 -0400

    new changes

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -6,14 +6,27 @@ public class InsertionSort {
         int arrlen = arr.length;
         for(int i=1;i<arrlen;i++){
             int pres = arr[i];
-            int counter = i;
-            while(pres>arr[counter-1]){
-                //int temp =
+            int counter = i-1;
+            while(counter>-1 && pres<arr[counter]){
+                arr[counter+1]=arr[counter];
+                counter--;
             }
+            arr[counter+1] = pres;
         }
     }
 
-    public static void main(String[] args) {
+    static void printArray(int arr[], int size)
+    {
+        int i;
+        for (i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
 
+    public static void main(String[] args) {
+        int arr[] = { 12, 11, 13, 5, 6 };
+        insertionSort(arr);
+
+        printArray(arr,arr.length);
     }
 }
