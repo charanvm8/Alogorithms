354280d6c0b5a9bbc67ceda138c4d3dc8188343a
charanvm8 <charanvm8@gmail.com>
2019-09-16 14:17:52 -0400

    new changes

--- src/sort/MergeSortRecursive.java
+++ src/sort/MergeSortRecursive.java
@@ -10,7 +10,7 @@ public class MergeSortRecursive {
 
     public void sort(int[] arr,int start,int end){
         if(end>start){
-            int mid = (end+start)/2;
+             int mid = (end+start)/2;
             sort(arr,0,mid);
             sort(arr,mid+1,end);
             merge(arr,start,mid,end);
@@ -18,24 +18,43 @@ public class MergeSortRecursive {
     }
 
     public static void merge(int[] arr,int start,int mid,int end){
-        int fArrLen = mid-start;
-        int sArrLen = (end-mid)-1;
-        int[] fArr = new int[fArrLen];
-        int[] sArr = new int[sArrLen];
-        for(int i=0;i<=fArrLen;i++)
-            fArr[i] = arr[start+i];
+        System.out.println("inside merge");
+        int firstArrSize = (mid-start)+1;
+        int secondArrSize = end-mid;
+        int[] firstArr = new int[firstArrSize];
+        int[] secondArr = new int[secondArrSize];
+        for(int i=0;i<firstArrSize;i++){
+            firstArr[i] = arr[start+i];
+        }
+        for(int i=0;i<secondArrSize;i++){
+            secondArr[i] = arr[mid+i+1];
+        }
+        int firstIndex = 0;
+        int secondIndex = 0;
+        int arrIndex = start;
+        while(firstIndex<firstArrSize && secondIndex<secondArrSize){
+            if(firstArr[firstIndex]<secondArr[secondIndex]){
+                arr[arrIndex++] = firstArr[firstIndex++];
+            }
+            else{
+                arr[arrIndex++] = secondArr[secondIndex++];
+            }
+        }
+        for(int z = firstIndex;z<firstArrSize;z++){
+            arr[arrIndex++] = firstArr[firstIndex++];
+        }
 
-        for(int k=0;k<=sArrLen;k++)
-            sArr[k] = arr[mid+1+k];
+        for(int z = secondIndex;z<secondArrSize;z++){
+            arr[arrIndex++] = secondArr[secondIndex++];
+        }
 
-        //while()
     }
 
     public static void main(String[] args) {
         int arr[] = {12, 11, 13, 5, 6, 7};
 
-        System.out.println("Given Array");
-        printArray(arr);
+//        System.out.println("Given Array");
+//        printArray(arr);
 
         MergeSortRecursive ob = new MergeSortRecursive();
         ob.sort(arr, 0, arr.length-1);
